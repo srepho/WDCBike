f3ab6be45951849f3c2618cec31a05952857f65c
srepho <stephen.j.a.oates@gmail.com>
2014-11-19 22:44:26 +1100

    combining

--- Script.R
+++ Script.R
@@ -4,4 +4,23 @@ library(caret)
 library(forecast)
 library(lubridate)
 
-train<-read.csv("E:kaggle/Bike/WDCBike/train.csv", )
+train<-read.csv("E:/kaggle/Bike/WDCBike/train.csv", header=T)
+test<-read.csv("E:/kaggle/Bike/WDCBike/test.csv", header=T)
+
+test1<-test
+
+test1<- test1 %>%
+  mutate(
+    casual=NA,
+    registered=NA,
+    count=NA
+  )
+
+
+
+Total<-rbind(train, test)
+
+
+ggplot(train, )
+
+
